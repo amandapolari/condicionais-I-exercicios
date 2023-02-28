b84462cb8680cefa2632d79daec06805cb6a627d
Amanda <amandaoliveirapolari@gmail.com>
2023-02-28 11:43:55 -0300

    Exercício 03 -> ok!

---
+++ exercicio3/script.js
@@ -0,0 +1,22 @@
+const nacionalidade = prompt('Escreva aqui sua nacionalidade');
+const nacionalidadeCorrigida = nacionalidade.toLocaleLowerCase();
+
+function verificaNacionalidade(valorNacionalidade) {
+    let mensagem = `A nacionalidade no usuário é ${valorNacionalidade}`
+    
+    if(valorNacionalidade==='brasileira'){
+        console.log(mensagem);
+    }else if(valorNacionalidade==='argentina') {
+        console.log(mensagem);
+    }else if(valorNacionalidade==='uruguaia') {
+        console.log(mensagem);
+    }else if(valorNacionalidade==='chilena') {
+        console.log(mensagem);
+    }else if(valorNacionalidade==='colombiana') {
+        console.log(mensagem);
+    } else {
+        console.log('Nacionalidade não encontrada')
+    }
+}
+
+verificaNacionalidade(nacionalidadeCorrigida)
