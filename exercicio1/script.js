8774059912b28f15e8b336aedf92e643400c4353
Amanda <amandaoliveirapolari@gmail.com>
2023-02-28 19:45:06 -0300

    Outro jeito de responder

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,13 +1,16 @@
 // EXERCÍCIO 1:
-// const numero =  Number(prompt('Por favor digite um número'));
-const numero =  4
+const numero =  Number(prompt('Por favor digite um número'));
+// const numero =  4
 
 function recebeNumero(num) {
+   let frase
    if (num % 2 === 0) {
     const mensagem = 'O número é par';
-    return 'é par';
+    frase = 'é par';
+    return frase
    }
-   return 'é ímpar'
+   frase = 'é ímpar'
+   return frase
 }
 
 console.log(recebeNumero(numero));
